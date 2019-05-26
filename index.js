24a9fc34f70ee247f23ea24efd06dba7f52ad0ff
Devansh <kdevanshsharma23@gmail.com>
2019-05-26 18:04:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+ /*toggle menu functinality*/
+function myFunction(x) {
+  x.classList.toggle("change");
+   
+}
+/* Secondary navigation bar property*/
+function myFunctio() {
+  var popup = document.getElementById("pop");
+  popup.classList.toggle("show");
+}
